d35385e5673662d25f6179dede0615edeaa13378
Nelofar Zabi <mutau85@gmail.com>
2023-08-19 17:49:35 +0200

    Add recipe and recipelist to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :recipe_foods
+  resources :recipes
   # Defines the root path route ("/")
   root to: 'foods#index'
 
